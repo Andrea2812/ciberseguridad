82cec2e8838288b3166dcde2d8f0308fb926773b
Andrea2812 <88693655+Andrea2812@users.noreply.github.com>
2021-08-10 21:48:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, Response
 import requests
 import json
+import logging
 
-app = Flask(__name__)
+app = Flask(_name_)
+
+
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 @app.route("/")
 def hello_world():
@@ -19,6 +25,8 @@ def catfact():
     print("Did you know?: " + printable_fact)
     return Response(json.dumps(fact))
 
+
+
 @app.route("/get-price/<ticker>")
 def get_price(ticker):
     url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
@@ -26,22 +34,33 @@ def get_price(ticker):
     response = requests.get(url,headers=headers)
     company_info = response.json()
 
-    print(company_info)
+    if response.status_code > 400:
+        app.logger.info(f"Yahoo has problem with ticker: {ticker}.")
+        app.logger.info(f"Yahoo status code: {response.status_code}.")
+        return Response({}, status=404, mimetype='application/json')
 
-    price = company_info['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
-    company_name = company_info['quoteSummary']['result'][0]['price']['longName']
-    exchange = company_info['quoteSummary']['result'][0]['price']['exchangeName']
-    currency = company_info['quoteSummary']['result'][0]['price']['currency']
+    app.logger.info(company_info)
 
-    result = {
-        "price": price,
-        "name": company_name,
-        "exchange": exchange,
-        "currency": currency
-    }
-    print(result)
+    try:
+        price = company_info['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
+        company_name = company_info['quoteSummary']['result'][0]['price']['longName']
+        exchange = company_info['quoteSummary']['result'][0]['price']['exchangeName']
+        currency = company_info['quoteSummary']['result'][0]['price']['currency']
 
-    return Response(json.dumps(result))
+        result = {
+            "price": price,
+            "name": company_name,
+            "exchange": exchange,
+            "currency": currency
+        }
+        app.logger.info(result)
 
-if __name__ == '__main__':
+        return Response(json.dumps(result), status=200, mimetype='application/json')
+    except (KeyError, TypeError):
+        return Response({}, status=404, mimetype='application/json')
+    except Exception as e:
+        app.logger.error("Exception occurred", exc_info=True)
+
+
+if _name_ == 'main':
     app.run()
