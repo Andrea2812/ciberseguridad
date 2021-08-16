83e53d86b68273b1f3b05183e179752d51c080f7
Andrea2812 <88693655+Andrea2812@users.noreply.github.com>
2021-08-15 20:20:42 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,16 @@
-# from app import catfact
+from app import get_price
 
-# def test_catfact():
-#     fact = catfact().json
-#     print(fact)
 
-#     assert fact['length'] > 0
+def test_get_price():
+    cocacola_price = get_price('KO').json
+    print(cocacola_price)
 
-def test_catfact():
-    pass
+    assert cocacola_price['price'] > 0
+    assert cocacola_price['name'] == 'The Coca-Cola Company'
+    assert cocacola_price['exchange'] == 'NYSE'
+    assert cocacola_price['currency'] == 'USD'
 
-test_catfact()
+    assert get_price('KSLAFSADF').status_code == 404
+
+
+test_get_price()
